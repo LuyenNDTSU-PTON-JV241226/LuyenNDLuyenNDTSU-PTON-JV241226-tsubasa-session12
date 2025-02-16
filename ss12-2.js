def55d9f19bbc338ee5e248fee97b0f7ed3c5b77
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-16 23:03:40 +0900

    upload btvn ss12-2

---
+++ ss12-2.js
@@ -0,0 +1,37 @@
+//ss12-2.1
+
+let c = +prompt("Hãy nhập vào số độ C");
+let f = (c*1.8) + 32;
+alert("độ F sẽ là: " + f + "độ F")
+
+//ss12-2.2
+
+let m = +prompt("Hãy nhập vào số mét");
+let feet = (m * 3.2808);
+alert ("số feet sẽ là: " + feet + "feet");
+
+//ss12-2.3
+
+let a = +prompt("Hãy nhập vào cạnh a của hình vuông");
+alert("diện tích hình vuông là: " + a*a);
+
+//ss12-2.4
+
+let a1 = +prompt("Hãy nhập vào chiều dài a hình chữ nhật");
+let b = +prompt("Hãy nhập vào chiều rộng b hình chữ nhật");
+alert("diện tích hình chữ nhật là: " + a1 * b);
+
+//ss12-2.5
+
+let a2 = +prompt("Hãy nhập vào cạnh a của tam giác");
+let b2 = +prompt("Hãy nhập vào cạnh b của tam giác");
+alert("diện tích tam giác là: " + a1 * b / 2);
+
+//ss12-2.8
+ 
+let z = +prompt("Hãy nhập vào số nguyên bạn muốn");
+if (z > 0 && z <= 120){
+    alert("tuổi của bạn là: " + z + "tuổi");
+}
+
+
